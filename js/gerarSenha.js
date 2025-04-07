8ea6b4e2babb275870a2c6162d454f34bd27c4ce
nogD3V <nogd3v@gmail.com.br>
2025-04-07 18:18:25 -0300

    Funcionalidade de gerar senha com JS | O botão parou de funcionar, tentando resolver

--- js/gerarSenha.js
+++ js/gerarSenha.js
@@ -1,12 +1,10 @@
-function gerarSenha(tamanho = 12) {
-    const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*";
+function gerarSenha() {
+    const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
     let senha = "";
-  
-    for (let i = 0; i < tamanho; i++) {
+    for (let i = 0; i < 12; i++) {
       const index = Math.floor(Math.random() * caracteres.length);
       senha += caracteres[index];
     }
-  
     document.getElementById("senhaGerada").value = senha;
   }
   
