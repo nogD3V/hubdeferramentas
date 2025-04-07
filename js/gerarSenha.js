0ac2a38f84a555ec9bcf7620f44472bbbeed926e
nogD3V <nogd3v@gmail.com.br>
2025-04-07 18:06:08 -0300

    Funcionalidade de gerar senha com JS | Adicionando o OCULTAR e a funcionalidade de sumir e aparecer

--- js/gerarSenha.js
+++ js/gerarSenha.js
@@ -12,6 +12,14 @@ function gerarSenha(tamanho = 12) {
   
   function toggleGerador() {
     const box = document.getElementById("geradorSenhaBox");
+    const botao = document.getElementById("toggleGeradorBtn");
+  
     box.classList.toggle("oculto");
+  
+    if (!box.classList.contains("oculto")) {
+      botao.textContent = "Ocultar";
+    } else {
+      botao.textContent = "Usar";
+    }
   }
   
